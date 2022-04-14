c7eb898f354285643e617776a4905699c5f69558
wahyuwww <wahyu.aan2508@gmail.com>
2022-04-14 11:10:52 +0700

    update

--- Task2.js
+++ Task2.js
@@ -3,23 +3,23 @@ const mtk = 100;
 const bhs_indo = 100;
 const bahasaInggris = 90;
 
-if (ipa !== "" || mtk !== "" || bhs_indo !== "" || bahasaInggris !== "") {
-   let mapel = [ipa, mtk, bhs_indo, bahasaInggris];
-   let Avg = 0;
-   let count = mapel.length;
-
-   for (let i = 0; i < count; i++) {
-     Avg += mapel[i];
-   }
-
-   console.log(`nilai rata-rata adalah : ${Avg / count}`);
-   let nilai = Avg / count;
-   if (nilai >= 90) console.log("Grade : A");
-   else if (nilai >= 80) console.log("Grade : B");
-   else if (nilai >= 70) console.log("Grade : C");
-   else if (nilai >= 60) console.log("Grade : D");
-   else if (nilai >= 0) console.log("Grade : E");
- 
-} else {
+if (ipa === "" || mtk === "" || bhs_indo === "" || bahasaInggris === "")  {
   console.log(" isi data dengan benar");
+} else {
+  let mapel = [ipa, mtk, bhs_indo, bahasaInggris];
+  let Avg = 0;
+  let count = mapel.length;
+
+  for (let i = 0; i < count; i++) {
+    Avg += mapel[i];
+  }
+  let nilai = Avg / count;
+  
+  console.log(`nilai rata-rata adalah : ${nilai}`);
+  
+  if (nilai >= 90) console.log("Grade : A");
+  else if (nilai >= 80) console.log("Grade : B");
+  else if (nilai >= 70) console.log("Grade : C");
+  else if (nilai >= 60) console.log("Grade : D");
+  else if (nilai >= 0) console.log("Grade : E");
 }
